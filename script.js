e7d40b95171e70b4e933ac9715ec813ecfd27245
Armin Sadr <arminsadr164@hotmail.com>
2023-01-23 19:20:58 +0100

    First Branch Commit

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const resultcontainer = document.querySelector(".resultdiv");
+resultcontainer.innerText = "The current score stands at" + "\n" + "Player: " + playerScore + "\n" + "Computer: " + computerScore;
+
+
 function getComputerChoice() {
     const options = ["Rock", "Paper", "Scissors"];
     let randomOption = options[Math.floor(Math.random()* options.length)].toUpperCase();
@@ -9,46 +13,42 @@ function getComputerChoice() {
 
 function playRound (playerSelection, computerSelection) {
     
-    if (playerSelection == computerSelection) {
-        console.log( "It¨s a draw");
+    if (playerSelection == computerSelection) {                                           
+        console.log( "It's a draw");
     }
 
     else if (
-        (playerSelection == "ROCK" && computerSelection == "SCISSORS")||
-        (playerSelection == "SCISSORS" && computerSelection == "PAPER")||
-        (playerSelection == "PAPER" && computerSelection == "ROCK") ) 
+        (playerSelection == "ROCK" && computerSelection == "SCISSORS")||                  
+        (playerSelection == "SCISSORS" && computerSelection == "PAPER")||               
+        (playerSelection == "PAPER" && computerSelection == "ROCK") )                     
         {
             playerScore++;
             console.log( "Player Wins");
+             resultcontainer.innerText = "The current score stands at" + "\n" + "Player: " + playerScore + "\n" + "Computer: " + computerScore;
         }
         
         else {
             computerScore++;
             console.log( "Computer Wins");
+             resultcontainer.innerText = "The current score stands at" + "\n" + "Player: " + playerScore + "\n" + "Computer: " + computerScore;
         }
+        
+        if (playerScore === 5 && playerScore > computerScore) {
+            console.log("PLAYER WON");
+            return resultcontainer.innerText = `PLAYER WINS with ${playerScore} pts`;
+        }
+    
+        else if (computerScore === 5 && playerScore < computerScore) {
+            console.log("COMPUTER WON");
+            return resultcontainer.innerText = `COMPUTER WINS with ${computerScore} pts`;
+        }
+        
 }
 
-
-
 function game() {
-    for( i = 0; i < 5; i++) {
-        let playerSelection = prompt(`Write your move. Player has ${playerScore}pts and computer has ${computerScore}pts`).toUpperCase();
-      
-        let computerSelection = getComputerChoice();
-
-        playRound(playerSelection, computerSelection);
-    }
-    
-    if (playerScore > computerScore) {
-        console.log(`PLAYER WINS WITH ${playerScore}pts`);
-    }
-
-    else if (playerScore < computerScore) {
-        console.log(`COMPUTER WINS WITH ${computerScore}pts`);
-    }
-
-    else {
-        console.log("THE GAME CONCLUDES WITH A DRAW");
-    }
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => playRound(button.innerText.toUpperCase(), getComputerChoice()));
+    });
 }
 game();
